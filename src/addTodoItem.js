e120f79b473a90b7e14c1d6c6de97825ae26049b
Юля Шишка <@shyshka20>
2022-02-10 13:39:06 +0200

    Delete item from list of elements

--- src/addTodoItem.js
+++ src/addTodoItem.js
@@ -1,3 +1,5 @@
+import { getTodosFromSStorage, removeTodoFromSStorage } from "./sessionStorage";
+
 export const getTodoItem = (text) => {
   // Create Todo Item
   const todoItem = document.createElement("li");
@@ -14,7 +16,27 @@ export const getTodoItem = (text) => {
   const removeButton = document.createElement("button");
   removeButton.innerHTML = "<i class='fas fa-times'></i>";
   removeButton.classList.add("todo-remove-button");
-
+  removeButton.addEventListener("click", removeTodoItem(todoItem));
   todoItem.appendChild(removeButton);
   return todoItem;
 };
+function removeTodoItem(todoItem) {
+  return (e) => {
+    e.preventDefault();
+    let result = confirm("Are you sure?");
+    if (result) {
+      todoItem.addEventListener("click", function () {
+        removeTodoFromSStorage(todoItem);
+        todoItem.remove();
+        disableSubmitButton();
+      });
+    }
+  };
+}
+function disableSubmitButton() {
+  const todos = getTodosFromSStorage();
+  if (!todos.length)
+    document
+      .querySelector(".submit-button")
+      .classList.add("submit-button_disabled");
+}
